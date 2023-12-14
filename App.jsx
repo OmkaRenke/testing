581c5fbae36559f7321b5562dbd36e133843f42c
OmkaRenke <110094880+OmkaRenke@users.noreply.github.com>
2023-12-14 12:43:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,41 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='logos'>
+        <div className='logos-slide'>
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+        </div>
+        <div className='logos-slide'>
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+          <img src="https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg" alt="" />
+        </div>
+      </div>
+  )
+}
+
+export default App
